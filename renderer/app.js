bf4403b1612d5b7565c866111eb12352e44b4292
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2026-01-02 15:19:16 +0800

    Check if url exists

--- renderer/app.js
+++ renderer/app.js
@@ -1,7 +1,9 @@
 // DOM Nodes
 let showModal = document.getElementById('show-modal'),
     closeModal = document.getElementById('close-modal'),
-    modal = document.getElementById('modal')
+    modal = document.getElementById('modal'),
+    addItem = document.getElementById('add-item'),
+    itemUrl = document.getElementById('url')
 
 // Show modal
 showModal.addEventListener('click', e => {
@@ -11,4 +13,14 @@ showModal.addEventListener('click', e => {
 // Hide modal
 closeModal.addEventListener('click', e => {
     modal.style.display = 'none'
+})
+
+// Handle new item
+addItem.addEventListener('click', e => {
+
+    // Check url exists
+    if (itemUrl.value) {
+        console.log(itemUrl.value)
+    }
+
 })
