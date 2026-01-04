aa59534d02cb46c8945d2e25aefcc86d9a7f5664
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2026-01-04 23:56:46 +0800

    Disable addItem button while fetching

--- renderer/app.js
+++ renderer/app.js
@@ -7,6 +7,24 @@ let showModal = document.getElementById('show-modal'),
     addItem = document.getElementById('add-item'),
     itemUrl = document.getElementById('url')
 
+// Disable and enable modal buttons
+const toggleModalButtons = () => {
+
+    // Check state of buttons
+    if (addItem.disabled === true) {
+        addItem.disabled = false
+        addItem.style.opacity = 1
+        addItem.innerText = 'Adding Item'
+        closeModal.style.display = 'inline'
+    } else {
+        addItem.disabled = true
+        addItem.style.opacity = 0.5
+        addItem.innerText = 'Adding...'
+        closeModal.style.display = 'none'
+    }
+
+}
+
 // Show modal
 showModal.addEventListener('click', e => {
     modal.style.display = 'flex'
@@ -23,15 +41,29 @@ addItem.addEventListener('click', e => {
 
     // Check url exists
     if (itemUrl.value) {
+
         // Send new item url to main process
         ipcRenderer.send('new-item', itemUrl.value)
+
+        // Disable buttons
+        toggleModalButtons()
+
     }
 
 })
 
 // Listen for new item from main process
 ipcRenderer.on('new-item-success', (e, newItem) => {
+
     console.log(newItem)
+
+    // Disable buttons
+    toggleModalButtons()
+
+    // Hide modal and clear input value
+    modal.style.display = 'none'
+    itemUrl.value = ''
+
 })
 
 // Listen for keyboard input
