ad08a24c8dc97a3623f621944bad7cba622afb57
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2026-01-11 19:47:39 +0800

    Persist item in local storage

--- renderer/items.js
+++ renderer/items.js
@@ -1,6 +1,14 @@
 // Get existing DOM node in main.html
 let items = document.getElementById('items')
 
+// Track items in local storage
+exports.storage = JSON.parse(localStorage.getItem('readit-items')) || []
+
+// Persist storage
+exports.save = () => {
+    localStorage.setItem('readit-items', JSON.stringify(this.storage))
+}
+
 exports.addItem = item => {
 
     // Create a new DOM node
@@ -10,5 +18,9 @@ exports.addItem = item => {
     itemNode.innerHTML = `<img src="${item.screenshot}"><h2>${item.title}</h2>`
 
     items.appendChild(itemNode)
+
+    // Add item to storage and persist
+    this.storage.push(item)
+    this.save()
 
 }
